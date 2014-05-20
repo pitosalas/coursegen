244684aa9560810b436b58c3becac63d23ccc82b
Pito Salas <pitosalas@gmail.com>
2014-05-20 16:42:20 -0400

    adding a few commands

--- lib/coursegen/cli.rb
+++ lib/coursegen/cli.rb
@@ -1,10 +1,15 @@
 require 'thor'
 module CourseGen
   class CLI < Thor
+    include Thor::Actions
 
-    desc "Help name", " Simply display name and prove that a command works at all"
+    desc "Greet NAME", " Simply display name and prove that a command works at all"
     def help(name)
       puts "Hello #{name}!!"
+    end
+
+    desc "new COURSE", "Create a new course by calling nanoc. Argument is name of the COURSE"
+    def new(course)
     end
   end
 end
