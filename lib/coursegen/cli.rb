1f106818492916f4e070840c1cda0148db378c8b
Pito Salas <pitosalas@gmail.com>
2014-05-20 14:44:08 -0400

    added chagne to execautbales

--- lib/coursegen/cli.rb
+++ lib/coursegen/cli.rb
@@ -2,7 +2,7 @@ require 'thor'
 module CourseGen
   class CLI < Thor
 
-    desc "Test name", " Simply display name and prove that a command works at all"
+    desc "Help name", " Simply display name and prove that a command works at all"
     def help(name)
       puts "Hello #{name}!!"
     end
