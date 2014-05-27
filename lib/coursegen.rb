3c7bd380e56fea88a6892554bc10bccdc4313dc8
Pito Salas <pitosalas@gmail.com>
2014-05-27 17:45:44 -0400

    small change

--- lib/coursegen.rb
+++ lib/coursegen.rb
@@ -1,5 +1,6 @@
 require "coursegen/version"
 require "coursegen/course/sidebar_helpers"
+require "coursegen/lecture_helpers"
 
 module Coursegen
   # Your code goes here...
---
+++ lib/coursegen/course/lecture_helpers.rb
@@ -0,0 +1,9 @@
+module LectureHelpers
+  def lecture_number_str citem
+    citem.lecture_number.to_s
+  end
+
+  def lecture_date
+    "-ddd-"
+  end
+end
