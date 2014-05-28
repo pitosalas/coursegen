56f347ec2d1d9e510ba1ba18265baf8589abf33f
Pito Salas <pitosalas@gmail.com>
2014-05-28 09:55:23 -0400

    coursegne.rb

--- lib/coursegen.rb
+++ lib/coursegen.rb
@@ -2,7 +2,7 @@ require "coursegen/version"
 require "coursegen/course/helpers/sidebar_helpers"
 require "coursegen/course/helpers/lecture_helpers"
 require "coursegen/course/helpers/content_helpers"
-require "coursegen/course/helperslogging_helpers"
+require "coursegen/course/helpers/logging_helpers"
 require "coursegen/course/helpers/navigation_helpers"
 
 module Coursegen
