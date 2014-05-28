fbe1cb1ada253bcdd5aaf7702592b42f1c51332a
Pito Salas <pitosalas@gmail.com>
2014-05-28 10:10:36 -0400

    coursegne.rb

--- lib/coursegen.rb
+++ lib/coursegen.rb
@@ -4,6 +4,8 @@ require "coursegen/course/helpers/lecture_helpers"
 require "coursegen/course/helpers/content_helpers"
 require "coursegen/course/helpers/logging_helpers"
 require "coursegen/course/helpers/navigation_helpers"
+require "coursegen/course/lib/toc"
+
 
 module Coursegen
   # Your code goes here...
