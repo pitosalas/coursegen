d6b30354cb950d77aa481d051af61c05bb3fd9bb
Pito Salas <pitosalas@gmail.com>
2014-07-18 17:51:51 -0400

    moved location of chalkmark helper

--- lib/coursegen.rb
+++ lib/coursegen.rb
@@ -7,6 +7,7 @@ require "coursegen/course/helpers/sidebar_helpers"
 require "coursegen/course/helpers/lecture_helpers"
 require "coursegen/course/helpers/content_helpers"
 require "coursegen/course/helpers/logging_helpers"
+require "coursegen/course/helpers/chalkmark_helpers"
 require "coursegen/course/helpers/list_of_helpers"
 require 'coursegen/course/helpers/bootstrap_markup'
 require "coursegen/course/helpers/navigation_helpers"
