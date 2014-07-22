44bb03695b3ef81da816507589266df86cb809a7
Pito Salas <pitosalas@gmail.com>
2014-07-22 17:32:00 -0400

    begin integrating tipeu

---
+++ templates/content/tipeusearch/demos/drop/tipuedrop/tipuedrop_content.js
@@ -0,0 +1,5 @@
+var tipuedrop = {"pages": [
+     {"title": "Tipue Search", "thumb": "img/search.png", "text": "Tipue Search, a site search engine jQuery plugin", "tags": "JavaScript js", "loc": "http://www.tipue.com/search"},
+     {"title": "Tipue drop", "thumb": "img/drop.png", "text": "Tipue drop, a search suggestion box jQuery plugin", "tags": "JavaScript js", "loc": "http://www.tipue.com/drop"},
+     {"title": "Tipr", "thumb": "img/tipr.png", "text": "Tipr, a small and simple tooltip jQuery plugin", "tags": "Tipue", "loc": "http://www.tipue.com/tipr"}
+]};
---
+++ templates/content/tipeusearch/demos/live/tipuesearch/tipuesearch_set.js
@@ -0,0 +1,23 @@
+
+/*
+Tipue Search 4.0
+Copyright (c) 2014 Tipue
+Tipue Search is released under the MIT License
+http://www.tipue.com/search
+*/
+
+
+var tipuesearch_stop_words = ["and", "be", "by", "do", "for", "he", "how", "if", "is", "it", "my", "not", "of", "or", "the", "to", "up", "what", "when"];
+
+var tipuesearch_replace = {"words": [
+     {"word": "tipua", "replace_with": "tipue"},
+     {"word": "javscript", "replace_with": "javascript"}
+]};
+
+var tipuesearch_stem = {"words": [
+     {"word": "e-mail", "stem": "email"},
+     {"word": "javascript", "stem": "script"},
+     {"word": "javascript", "stem": "js"}
+]};
+
+var tipuesearch_pages = ["http://www.tipue.com/search", "http://www.tipue.com/search/demos/static",  "http://www.tipue.com/search/demos/live", "http://www.tipue.com/blog", "http://www.tipue.com/tipr", "http://www.tipue.com/jquery", "http://www.tipue.com/support", "http://www.tipue.com/drop", "http://www.tipue.com/about", "http://www.tipue.com/tos"];
