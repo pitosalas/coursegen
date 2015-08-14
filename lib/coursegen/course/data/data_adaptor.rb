0d00b5956768133bec9c524b97fca84611a35423
Pito Salas <pitosalas@gmail.com>
2015-08-13 21:37:40 -0400

    fixed string for full_desc

--- lib/coursegen/course/data/data_adaptor.rb
+++ lib/coursegen/course/data/data_adaptor.rb
@@ -35,7 +35,9 @@ class DataAdaptor
     when :end_date_time
       citem.schedule_end_date_time
     when :full_desc
-      "this is a lot of work! Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      desc = citem.desc || ""
+      hw = citem.homework || ""
+      desc + ". HW: " + hw
     else
       "error!"
     end
