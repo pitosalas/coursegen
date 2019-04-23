5b0b21bc8b5884a7f300dfd0e215ccac867d00ad
Pito Salas <pitosalas@gmail.com>
2019-04-23 10:28:48 -0400

    updating it more

--- templates/content/bootstrap/js/tipuesearch_set.js
+++ templates/content/bootstrap/js/tipuesearch_set.js
@@ -1,7 +1,7 @@
 
 /*
-Tipue Search 4.0
-Copyright (c) 2014 Tipue
+Tipue Search 3.1
+Copyright (c) 2013 Tipue
 Tipue Search is released under the MIT License
 http://www.tipue.com/search
 */
@@ -10,13 +10,14 @@ http://www.tipue.com/search
 var tipuesearch_stop_words = ["and", "be", "by", "do", "for", "he", "how", "if", "is", "it", "my", "not", "of", "or", "the", "to", "up", "what", "when"];
 
 var tipuesearch_replace = {"words": [
-     {"word": "tipua", "replace_with": "tipue"},
-     {"word": "javscript", "replace_with": "javascript"}
+     {"word": "tipua", replace_with: "tipue"},
+     {"word": "javscript", replace_with: "javascript"}
 ]};
 
 var tipuesearch_stem = {"words": [
-     {"word": "e-mail", "stem": "email"},
-     {"word": "javascript", "stem": "script"},
-     {"word": "javascript", "stem": "js"}
+     {"word": "e-mail", stem: "email"},
+     {"word": "javascript", stem: "script"},
+     {"word": "javascript", stem: "js"}
 ]};
+
 
