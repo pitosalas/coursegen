cbe6a58d8ec1066615b156afc13df4867a72871a
Pito Salas <pitosalas@gmail.com>
2020-04-12 18:13:34 -0400

    tiny tinyy change

--- lib/coursegen/course/helpers/content_helpers.rb
+++ lib/coursegen/course/helpers/content_helpers.rb
@@ -120,7 +120,7 @@ module ContentHelpers
   end
 
   def image(filename_string, extra: "")
-    <<-HTMLSTRING
+    <<~HTMLSTRING
     <img src="/content/topics/images/#{filename_string}" class="img-responsive img-thumbnail" #{extra}/>
     HTMLSTRING
   end
