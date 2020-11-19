dcfe1a07d996175d01cf2ef72a6e3ee2d2156213
Pito Salas <pitosalas@gmail.com>
2020-11-19 15:50:51 -0500

    checkpoint

--- lib/coursegen/course/helpers/content_helpers.rb
+++ lib/coursegen/course/helpers/content_helpers.rb
@@ -1,5 +1,6 @@
 # ContentHelpers is used to annotate content.
 module ContentHelpers
+  
   def include_topic(item_symbol)
     incorporated_topic = lookup_nitem('topics', item_symbol.to_s)
     items[incorporated_topic.identifier.to_s].compiled_content
