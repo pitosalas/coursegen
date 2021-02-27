f9b9c69dd2a0a07c6be9b70fd1c3a965e98acc41
Pito Salas <pitosalas@gmail.com>
2021-02-27 14:35:36 -0500

    checkpoint

--- lib/coursegen/course/lib/nanoc_filters.rb
+++ lib/coursegen/course/lib/nanoc_filters.rb
@@ -2,8 +2,10 @@ include VisualHelpers
 
 Nanoc::Filter.define(:handle_shortcodes) do |content, _params = {}|
   content
-    .gsub(/^\$\$INCLUDE_TOPIC :(\w+)$/) { "<%= include_topic( :#{$1}) %>" }
-    .gsub(/^\$\$SLIDE_TITLE (.+)$/) { "<slide_break></slide_break>\n\n### #{$1}" }
+    .gsub(/^\$\$TOPIC_INCLUDE :(\w+)$/) { "<%= include_topic( :#{Regexp.last_match(1)}) %>" }
+    .gsub(/^\$\$SLIDE_TITLE (.+)$/) { "<slide_break></slide_break>\n\n### #{Regexp.last_match(1)}" }
     .gsub(/^\$\$SLIDE_BREAK$/) { "<slide_break></slide_break>" }
-    .gsub(/^\$\$END_OF_LECTURE$/) { end_of_lecture }
-end
+    .gsub(/^\$\$LECTURE_END$/) { end_of_lecture }
+    .gsub(/^\$\$LECTURE_BEGIN (.+)$/) { "## #{Regexp.last_match(1)} "}
+
+  end
