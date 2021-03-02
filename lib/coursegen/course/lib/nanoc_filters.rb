de5e36d183dbd32578b44e7444b48aa76c1f6716
Pito Salas <pitosalas@gmail.com>
2021-03-01 20:38:29 -0500

    checkpoint

--- lib/coursegen/course/lib/nanoc_filters.rb
+++ lib/coursegen/course/lib/nanoc_filters.rb
@@ -5,10 +5,10 @@ Nanoc::Filter.define(:handle_shortcodes) do |content, _params = {}|
   content
     .gsub(/^\$\$TOPIC_INCLUDE :(\w+)$/) { "<%= include_topic( :#{Regexp.last_match(1)}) %>" }
     .gsub(/\$\$TOPIC_LINK :(\w+)$/) { "<%= link_to_topic( :#{Regexp.last_match(1)}) %>" }
-    .gsub(/^\$\$SLIDE_TITLE (.+)$/) { "<slide_break></slide_break>\n\n### #{Regexp.last_match(1)}" }
-    .gsub(/^\$\$SLIDE_BREAK$/) { "<slide_break></slide_break>" }
-    .gsub(/^\$\$LECTURE_END$/) { end_of_lecture }
-    .gsub(/^\$\$LECTURE_BEGIN (.+)$/) { "## #{Regexp.last_match(1)} "}
+    .gsub(/\$\$SLIDE_TITLE (.+)$/) { "<slide_break></slide_break>\n\n### #{Regexp.last_match(1)}" }
+    .gsub(/\$\$SLIDE_BREAK$/) { "<slide_break></slide_break>" }
+    .gsub(/\$\$LECTURE_END$/) { end_of_lecture }
+    .gsub(/\$\$LECTURE_BEGIN (.+)$/) { "## #{Regexp.last_match(1)} "}
     .gsub(Regexp.new(prefix.source + /h1 (.+)$/.source), "# \\1 ")
     .gsub(Regexp.new(prefix.source + /h2 (.+)$/.source), "## \\1 ")
     .gsub(Regexp.new(prefix.source + /h3 (.+)$/.source), "### \\1 ")
