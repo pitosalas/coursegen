2377913a616b3c61f40515f10a9c4ea09303cc4f
Pito Salas <pitosalas@gmail.com>
2021-04-05 18:13:08 -0400

    checkpoint

--- lib/coursegen/course/lib/nanoc_filters.rb
+++ lib/coursegen/course/lib/nanoc_filters.rb
@@ -3,12 +3,6 @@ include VisualHelpers
 Nanoc::Filter.define(:handle_shortcodes) do |content, _params = {}|
   prefix = /:/
   content
-    .gsub(/^\$\$TOPIC_INCLUDE :(\w+)$/) { "<%= include_topic( :#{Regexp.last_match(1)}) %>" }
-    .gsub(/\$\$TOPIC_LINK :(\w+)$/) { "<%= link_to_topic( :#{Regexp.last_match(1)}) %>" }
-    .gsub(/\$\$SLIDE_TITLE (.+)$/) { "<slide_break></slide_break>\n\n### #{Regexp.last_match(1)}" }
-    .gsub(/\$\$SLIDE_BREAK$/) { "<slide_break></slide_break>" }
-    .gsub(/\$\$LECTURE_END$/) { end_of_lecture }
-    .gsub(/\$\$LECTURE_BEGIN (.+)$/) { "## #{Regexp.last_match(1)} "}
     .gsub(Regexp.new(prefix.source + /h1 (.+)$/.source), "# \\1 ")
     .gsub(Regexp.new(prefix.source + /h2 (.+)$/.source), "## \\1 ")
     .gsub(Regexp.new(prefix.source + /h3 (.+)$/.source), "### \\1 ")
@@ -16,6 +10,7 @@ Nanoc::Filter.define(:handle_shortcodes) do |content, _params = {}|
     .gsub(Regexp.new(prefix.source + /topic_link :(\w+)/.source)) { |match| link_to_topic($1) }
     .gsub(Regexp.new(prefix.source + /section_link :(\w+) :(\w+)/.source)) { |match| link_to_section($1, $2) }
     .gsub(Regexp.new(prefix.source + /slide_title (.+)$/.source), "<slide_break></slide_break>\n\n### \\1")
+    .gsub(Regexp.new(prefix.source + /slide_bigtitle (.+)$/.source), "<slide_break></slide_break>\n\n## \\1")
     .gsub(Regexp.new(prefix.source + /lecture_end/.source), end_of_lecture)
     .gsub(Regexp.new(prefix.source + /deliverable (.+)$/.source), "<%= deliverable( \"\\1\") %>")
     .gsub(Regexp.new(prefix.source + /pdfbadge/.source), pdfbadge)
