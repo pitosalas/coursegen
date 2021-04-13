743a543242db961ba138bca3f91c4989ddfdeb7c
Pito Salas <pitosalas@gmail.com>
2021-04-13 15:58:16 -0400

    checkpoint

--- lib/coursegen/course/lib/nanoc_filters.rb
+++ lib/coursegen/course/lib/nanoc_filters.rb
@@ -1,10 +1,10 @@
 include VisualHelpers
 
 Nanoc::Filter.define(:handle_shortcodes) do |content, _params = {}|
-    prefix = /:/
+  prefix = /:/
     content
-      .gsub(Regexp.new(prefix.source + /callout :(\w+), \"(.+)\", \"(.+)\"/.source)) { callout($2, $3, $1) }
-      .gsub(Regexp.new(prefix.source + /deliverable (.+)$/.source), "<%= deliverable( \"\\1\") %>")
+      .gsub(Regexp.new(prefix.source + /callout :(\w+), "(.+)", "(.+)"/.source)) { callout($2, $3, $1) }
+      .gsub(Regexp.new(prefix.source + /deliverable (.+)$/.source)) { deliverable($1) }
       .gsub(Regexp.new(prefix.source + /discussion_box (.+)$/.source), "<%= discussion_box( \"\\1\") %>")
       .gsub(Regexp.new(prefix.source + /h1 (.+)$/.source), "# \\1 ")
       .gsub(Regexp.new(prefix.source + /h2 (.+)$/.source), "## \\1 ")
@@ -21,7 +21,7 @@ Nanoc::Filter.define(:handle_shortcodes) do |content, _params = {}|
       .gsub(Regexp.new(prefix.source + /slide_title (.+)$/.source), "<slide_break></slide_break>\n\n### \\1")
       .gsub(Regexp.new(prefix.source + /source_begin :(.+)$/.source)) { source_begin( ":language-"+$1) }
       .gsub(Regexp.new(prefix.source + /source_end/.source), source_end)
-      .gsub(Regexp.new(prefix.source + /team_deliverable (.+)$/.source), "<%= team_deliverable( \"\\1\") %>")
+      .gsub(Regexp.new(prefix.source + /team_deliverable (.+)$/.source)) { team_deliverable($1) }
       .gsub(Regexp.new(prefix.source + /teambadge/.source), teambadge)
       .gsub(Regexp.new(prefix.source + /topic_include :(\w+)$/.source)) { include_topic($1) }
       .gsub(Regexp.new(prefix.source + /topic_link :(\w+)/.source)) { link_to_topic($1) }
