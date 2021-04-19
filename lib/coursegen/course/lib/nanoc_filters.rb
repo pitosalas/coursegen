e983b2ee99bed341d5d5f0f696b624b894be70fb
Pito Salas <pitosalas@gmail.com>
2021-04-19 14:11:30 -0400

    added better yellow stickeers

--- lib/coursegen/course/lib/nanoc_filters.rb
+++ lib/coursegen/course/lib/nanoc_filters.rb
@@ -10,9 +10,11 @@ Nanoc::Filter.define(:handle_shortcodes) do |content, _params = {}|
       .gsub(Regexp.new(prefix.source + /h2 (.+)$/.source), "## \\1 ")
       .gsub(Regexp.new(prefix.source + /h3 (.+)$/.source), "### \\1 ")
       .gsub(Regexp.new(prefix.source + /h4 (.+)$/.source), "#### \\1 ")
+      .gsub(Regexp.new(prefix.source + /h5 (.+)$/.source)) { "<h5>#{$1}</h5>" }
       .gsub(Regexp.new(prefix.source + /image :(\w+)$/.source)) { "<img src=\"/content/topics/images/#{$1}.jpg\" class=\"img-thumbnail\" style=\"height:300px;\"/>" }
       .gsub(Regexp.new(prefix.source + /image :(\w+), :(\w+)$/.source)) { "<img src=\"/content/topics/images/#{$2}.jpg\" class=\"img-thumbnail\" style=\"height:#{$1}px;\"/>" }
       .gsub(Regexp.new(prefix.source + /lecture_end/.source), end_of_lecture)
+      .gsub(Regexp.new(prefix.source + /li (.+)$/.source)) { "<li>#{$1}</li>" }
       .gsub(Regexp.new(prefix.source + /partbadge/.source), partbadge)
       .gsub(Regexp.new(prefix.source + /pdfbadge/.source), pdfbadge)
       .gsub(Regexp.new(prefix.source + /section_link :(\w+) :(\w+)/.source)) { link_to_section($1, $2) }
@@ -26,6 +28,9 @@ Nanoc::Filter.define(:handle_shortcodes) do |content, _params = {}|
       .gsub(Regexp.new(prefix.source + /teambadge/.source), teambadge)
       .gsub(Regexp.new(prefix.source + /topic_include :(\w+)$/.source)) { include_topic($1) }
       .gsub(Regexp.new(prefix.source + /topic_link :(\w+)/.source)) { link_to_topic($1) }
+      .gsub(Regexp.new(prefix.source + /ul_begin :(\w+)/.source)) { "<ul class=\"#{$1}\">" }
+      .gsub(Regexp.new(prefix.source + /ul_begin$/.source)) { "<ul>" }
+      .gsub(Regexp.new(prefix.source + /ul_end$/.source)) { "</ul>" }
       .gsub(Regexp.new(prefix.source + /youtube \"(.+)\"$/.source)) {  %Q!<iframe width="560" height="315" src="#{$1}"></iframe>! }
       .gsub(Regexp.new(prefix.source + /zipbadge/.source), zipbadge)
 end
--- lib/coursegen/version.rb
+++ lib/coursegen/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Coursegen
-  VERSION = '0.9.17'
+  VERSION = '0.9.18'
 end
