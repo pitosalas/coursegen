8d7c8f26c41a7f014a8cf814f7331b111983ab5a
Pito Salas <pitosalas@gmail.com>
2021-08-06 10:32:37 -0400

    fixed end_of_lecture

--- lib/coursegen/course/helpers/visual_helpers.rb
+++ lib/coursegen/course/helpers/visual_helpers.rb
@@ -2,7 +2,10 @@ module VisualHelpers
 
   def end_of_lecture(type=:thatsall)
     if (type == :thatsall)
-      "<slide_break></slide_break>" + "<h2>See You Next Time!</h2>" + include_image("/content/topics/images/thatsallfolks.jpg", extra: "d-block mx-auto")
+      part1 = "<slide_break></slide_break>\n\n"
+      part2 = "## See you next time!!\n\n"
+      part3 = "<img src=\"/content/topics/images/thatsallfolks.jpg\" class=\"img-thumbnail d-block mx-auto\" style=\"height:300px;\"/>"
+      part1 + part2 + part3
     end
   end
 end
