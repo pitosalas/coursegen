7911d74c6e654d6003f42adc19d85741d184a88c
Pito Salas <pitosalas@gmail.com>
2022-01-24 12:01:31 -0500

    checkpoint

--- lib/coursegen/course/helpers/visual_helpers.rb
+++ lib/coursegen/course/helpers/visual_helpers.rb
@@ -4,16 +4,17 @@ module VisualHelpers
       part1 = "<slide_break></slide_break>\n\n"
       part2 = "## See you next time!!\n\n"
       part3 = "<img src=\"/content/topics/images/thatsallfolks.jpg\" class=\"img-thumbnail d-block mx-auto\" style=\"height:300px;\"/>"
+      part4 = ""
     elsif type == :random
       part1 = "<slide_break></slide_break>\n\n"
-      part2 = "## Thank you. Questions?"
+      part2 = "<h2 class=\"shadow p-3 bg-white rounded\">Thank you. Questions?"
       part3 = random_image :normal, "800", "50"
+      part4 = "\</h2>"
     end
-    part1 + part2 + part3
+    part1 + part2 + part3 + part4
   end
 end
 
 def random_image(_style, width, height)
-  classes = "\"shadow p-3 bg-white rounded\""
-  "<img class=#{classes} src=\"https://picsum.photos/#{width}/#{height}.jpg\" /> "
+  "<img class=\"img-fluid\ w-100\" src=\"https://picsum.photos/#{width}/#{height}.jpg\" /> "
 end
